4f078d4ad1e2468e893277492b73e323d013af40
hunter <shaileshkharola0048@gmail.com>
2025-07-10 15:17:57 +0530

    basic

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,1 @@
-const text = "Shailesh Kharola";
-let i = 0;
 
-function typeEffect() {
-  if (i < text.length) {
-    document.getElementById("typed").textContent += text.charAt(i);
-    i++;
-    setTimeout(typeEffect, 100); // Typing speed (ms)
-  }
-}
-
-document.addEventListener("DOMContentLoaded", typeEffect);
